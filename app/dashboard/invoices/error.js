cd0c01c84f9c30abd215f969374d4b5d331f8257
senthilkumar <senthilkumar@responsive.io>
2024-12-18 20:56:49 +0530

    Initial commit

--- app/dashboard/invoices/error.js
+++ app/dashboard/invoices/error.js
@@ -5,9 +5,6 @@ import { useEffect } from 'react';
 export default function Error({
   error,
   reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
 }) {
   useEffect(() => {
     // Optionally log the error to an error reporting service
